a17d3fac0b624d3d85c82b24ef25e863c3d662b7
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-03-27 17:02:08 -0400

    add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import {Route, BrowserRouter as Router, Redirect} from "react-router-dom";
+import {HashRouter, Route, BrowserRouter as Router, Redirect} from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,24 +10,26 @@ import View from "./pages/View";
 
 function App() {
     return (
-        <div style={{fontFamily: "Nunito"}}>
-            <Router>
-                <Route path="/" exact component={Home}/>
-                <Route path="/about" component={About}/>
-                <Route path="/dashboard" component={Dashboard}/>
+        <HashRouter basename="/">
+            <div style={{fontFamily: "Nunito"}}>
+                <Router>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/dashboard" component={Dashboard}/>
 
-                <Route path="/edit" exact>
-                    <Redirect to="/"></Redirect>
-                </Route>
+                    <Route path="/edit" exact>
+                        <Redirect to="/"></Redirect>
+                    </Route>
 
-                <Route path="/view" exact>
-                    <Redirect to="/"></Redirect>
-                </Route>
+                    <Route path="/view" exact>
+                        <Redirect to="/"></Redirect>
+                    </Route>
 
-                <Route path="/edit/:id" component={Edit}/>
-                <Route path="/view/:id" component={View}/>
-            </Router>
-        </div>
+                    <Route path="/edit/:id" component={Edit}/>
+                    <Route path="/view/:id" component={View}/>
+                </Router>
+            </div>
+        </HashRouter>
     )
 }
 
